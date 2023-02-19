339e701df98418f6ce090dbcf61c759e6a538bc1
Yulia-Chamlai <yuliachamlai@gmail.com>
2023-02-19 22:26:12 +0200

    init repo

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".slider").slick({
+    prevArrow:
+      '<button class="slick-arrow slick-prev"><img src="images/slider-img/arrow-prev.svg" alt="prev arrow" /></button>',
+    nextArrow:
+      '<button class="slick-arrow slick-next"><img src="images/slider-img/arrow-next.svg" alt="next arrow" /></button>',
+  });
+});
